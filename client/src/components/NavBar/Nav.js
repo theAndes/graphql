07a35e201816318fea0a3610a202659e82c0a2a3
theAndes <age.esquivel@gmail.com>
2019-05-13 12:40:42 -0500

    <AuthContext.Provider             value={{               token: this.state.token,               userId: this.state.userId,               login: this.login,               logout: this.logout             }}

--- client/src/components/NavBar/Nav.js
+++ client/src/components/NavBar/Nav.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+import AuthContext from '../../context/auth-context';
 import './Nav.css';
-const navBar = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>Book Your Event</h1>
-    </div>
-    <nav className="main-navigation__items">
-      <ul>
-        <li>
-          <NavLink to="/auth">Authenticate</NavLink>
-        </li>
-        <li>
-          <NavLink to="/events">Events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bookings">Bookings</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+
+const mainNavigation = props => (
+  <AuthContext.Consumer>
+    {context => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>EasyEvent</h1>
+          </div>
+          <nav className="main-navigation__items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authenticate</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
-export default navBar;
+export default mainNavigation;
